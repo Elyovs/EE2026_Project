4b16f5971aaff8c4860d9a07b530beac996f098c
Ella Yovita Suwibowo <64566863+Elyovs@users.noreply.github.com>
2023-11-03 11:34:18 +0800

    add reset

--- clock_2_min.v
+++ clock_2_min.v
@@ -20,14 +20,34 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module clock_2_min(input clock, output reg clk_2m);
-
+module player_turn_2_min(input clock, btnC, output reg [1:0] led);
+    
     reg [31:0] count = 0;
     reg [31:0] count_6 = 0;
+    reg clk_2m = 0;
     wire [31:0] count_2d6m = 1_000_000_000;
+    reg reset_s = 0;
+    reg reset_l = 0;
     
     always @ (posedge clock) begin
-        count <= (count == count_2d6m) ? 0 : count + 1;
+        if (btnC) 
+        begin
+            reset_s <= 1;
+            reset_l <= 1;
+        end
+        
+        if (count == count_2d6m || reset_s)
+        begin
+            count <= 0;
+            reset_s <= 0;
+        end
+        else count <= count + 1;
+        
+        if (reset_l)
+        begin
+            reset_l <= 0;
+            count_6 <= 0;
+        end
         
         if (count == count_2d6m)
         begin
@@ -35,6 +55,10 @@ module clock_2_min(input clock, output reg clk_2m);
         end
         
         clk_2m <= (count_6 == 6) ? ~clk_2m : clk_2m;
+        
+        led[0] <= clk_2m;   //led toggle every 1 minute
+                            //on - off - on -> 2 min
+        
     end
     
 endmodule
