ebdce9b43ceef523ed3a0c8e0329875fe1a7ebe0
Ella Yovita Suwibowo <64566863+Elyovs@users.noreply.github.com>
2023-11-04 19:29:30 +0800

    Update clock_2_min.v

--- clock_2_min.v
+++ clock_2_min.v
@@ -20,32 +20,47 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module player_turn_2_min(input clock, btnC, output reg [2:0] led);
+module player_turn_1_min(
+    input clock, moved,
+    output reg player_turn, output reg end_game, output reg winner
+//    output reg [2:0] led, 
+);
     
     reg [31:0] count;
     reg [31:0] count_6;
-    reg [1:0] clk_2m;
+    reg [1:0] clk_1m;
     wire [31:0] count_2d6m = 49_999_999;
     reg [1:0] reset_s = 0;
     reg [1:0] reset_l = 0;
+    reg [31:0] move_count = 0;
     
-    reg [1:0] player = 0;
-    reg [1:0] end_game = 0;
+    
+    
+//    reg [1:0] player = 0;
+//    reg [1:0] end_game = 0;
     
     initial begin
         count <= 0;
         count_6 <= 0;
-        clk_2m <= 0;
+        clk_1m <= 0;
     end
     
     always @ (posedge clock) begin
-        if (btnC) 
+        if (moved) 
+//        if (btnC)
+        begin
+            move_count <= (move_count > 5) ? move_count : move_count + 1;
+        end
+        
+        if (moved == 0) move_count <= 0;
+        
+        if (move_count == 1)
         begin
             reset_s <= 1;
             reset_l <= 1;
-            player <= ~player;
+            player_turn <= ~player_turn;
         end
-        
+
         count <= (count == count_2d6m) ? 0 : count + 1;
         
         if (count == count_2d6m || reset_s)
@@ -63,20 +78,27 @@ module player_turn_2_min(input clock, btnC, output reg [2:0] led);
         
         if (count == count_2d6m)
         begin
-            count_6 <= (count_6 == 240) ? 0 : count_6 + 1;
-            clk_2m <= (count_6 == 240) ? ~clk_2m : clk_2m;
+            count_6 <= (count_6 == 120) ? 0 : count_6 + 1;
+            clk_1m <= (count_6 == 120) ? ~clk_1m : clk_1m;
         end
         
-        if (clk_2m)
+        if (count_6 == 120 && end_game == 0)
         begin
             end_game <= 1;
-        end 
+            winner <= clk_1m;
+        end
+        
+//        if (clk_2m)
+//        begin
+//            end_game <= 1;
+//            winner <= 
+//        end 
         
 //        led[1] <= clk_2m;   //led toggle every 1 minute
 //                            //on - off - on -> 2 min
-        led[0] <= ~player;
-        led[1] <= player;
-        led[2] <= end_game; //end game when 2 min passed
+//        led[0] <= ~player;
+//        led[1] <= player;
+//        led[2] <= end_game; //end game when 2 min passed
         
     end
     
